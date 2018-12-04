d600a1292424b89e953247a26265dbd230c1428b
Alec Clifford <alec.clifford@gmail.com>
2018-12-03 20:25:52 -0500

    Added Google Directions API service Added savings service

---
+++ marta/service/google_directions_api_service.py
@@ -0,0 +1,29 @@
+import googlemaps
+import os
+from datetime import datetime
+
+# TODO: Externalize this API key
+gmaps_client = googlemaps.Client(key=os.environ['GOOGLE_API_KEY'])
+
+
+def get_directions(origin, destination):
+    # Request directions via public transit
+    now = datetime.now()
+    directions_result = gmaps_client.directions(origin + ", GA",
+                                                destination + ", GA",
+                                                departure_time=now)
+    return directions_result
+
+
+def get_directions_total_distance(directions_result):
+    # All legs of trip
+    legs = directions_result[0]["legs"]
+    # Distance in miles
+    distance = 0
+    for leg in legs:
+        # Distance as text, ex. "11.8 mi"
+        leg_distance_text = leg['distance']['text']
+        # Distance as float
+        leg_distance = float(leg_distance_text[0:len(leg_distance_text) - 3])
+        distance += leg_distance
+    return distance
---
+++ marta/service/savings_service.py
@@ -0,0 +1,23 @@
+from marta.service import google_directions_api_service as directions_service
+
+# TODO: Make these dynamic / real-time
+dollars_per_gallon_gas = 2.40
+avg_miles_per_gallon = 24.7
+
+
+# Get total dollar savings for trip from origin to destination
+def get_dollar_savings_for_trip(origin, destination):
+    return distance_to_dollar_savings(get_trip_distance(origin, destination))
+
+
+# Calculate dollar savings from trip distance
+def distance_to_dollar_savings(distance):
+    dollar_savings = distance / avg_miles_per_gallon * dollars_per_gallon_gas
+    return dollar_savings
+
+
+# Get total trip distance using Google Maps API service
+def get_trip_distance(origin, destination):
+    directions_result = directions_service.get_directions(origin, destination)
+    distance = directions_service.get_directions_total_distance(directions_result)
+    return distance
